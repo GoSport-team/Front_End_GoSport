c9740b963d572ed24b884dedfc1540fc6f0119f1
darbincuene <celedon22cuene@gmail.com>
2024-07-22 18:15:23 -0500

    fron

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,5 @@
 export * from "@/pages/dashboard/home";
 export * from "@/pages/dashboard/perfil";
 export * from "@/pages/dashboard/tables";
+export *from "@/pages/dashboard/subirfoto";
 export * from "@/pages/dashboard/notifications";
