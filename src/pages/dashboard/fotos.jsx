7b7d9e04b7f1e6faf51d4d3b33e26c924386a25c
Jmigueldulcey <jmigueldulcey@gmail.com>
2024-07-29 18:25:07 -0500

    fotos

--- src/pages/dashboard/fotos.jsx
+++ src/pages/dashboard/fotos.jsx
@@ -21,6 +21,7 @@ export function Fotos() {
   const navigate = useNavigate();
 
   useEffect(() => {
+    setLoading(true);
     fetch('http://localhost:3001/photo')
       .then(response => {
         if (!response.ok) {
@@ -30,12 +31,13 @@ export function Fotos() {
       })
       .then(data => {
         setPhotos(data);
-        setLoading(false); // Set loading to false after data is successfully fetched
       })
       .catch(error => {
         console.error('Error fetching photos:', error);
         toast.error('Error fetching photos');
-        setLoading(false); // Set loading to false even if there is an error
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -45,6 +47,7 @@ export function Fotos() {
   };
 
   const handleConfirmDelete = () => {
+    setLoading(true);
     fetch(`http://localhost:3001/photo/${photoToDelete}`, {
       method: 'DELETE',
     })
@@ -53,12 +56,15 @@ export function Fotos() {
           throw new Error('Error al eliminar la foto');
         }
         setPhotos(photos.filter(photo => photo._id !== photoToDelete));
-        setShowConfirmDelete(false);
         toast.success('Foto eliminada');
       })
       .catch(error => {
         console.error('Error deleting photo:', error);
         toast.error('Error deleting photo');
+      })
+      .finally(() => {
+        setLoading(false);
+        setShowConfirmDelete(false);
       });
   };
 
@@ -104,9 +110,12 @@ export function Fotos() {
     const data = new FormData();
     data.append('Nombre', formData.Nombre);
     data.append('Descripcion', formData.Descripcion);
+    
     if (selectedFile) {
       data.append('image', selectedFile);
     }
+
+    setLoading(true);
 
     fetch('http://localhost:3001/photo', {
       method: 'POST',
@@ -120,24 +129,30 @@ export function Fotos() {
           Descripcion: ''
         });
         setSelectedFile(null);
-        setShowConfirm(false);
-        setShowUploadModal(false);
         toast.success('Foto subida con éxito');
       })
       .catch(error => {
         console.error('Error uploading photo:', error);
         toast.error('Error uploading photo');
+      })
+      .finally(() => {
+        setLoading(false);
+        setShowConfirm(false);
+        setShowUploadModal(false);
       });
   };
 
   return (
     <main className="flex flex-col items-center min-h-screen bg-blue-gray-50/50">
+      {/* Spinner de carga */}
+      {loading && (
+        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+          <Spinner size="lg" className="text-blue-600" />
+        </div>
+      )}
+
       <div className="w-full max-w-8xl mt-10">
-        {loading ? (
-          <div className='flex justify-center items-center h-72'>
-            <Spinner size="lg" className="text-blue-600" />
-          </div>
-        ) : (
+        {!loading && (
           <>
             <div className="flex justify-center w-full mb-5">
               <button 
@@ -176,118 +191,118 @@ export function Fotos() {
                 ))}
               </div>
             </div>
-          </>
-        )}
-      </div>
+            
+            {showUploadModal && (
+              <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="fixed inset-0 bg-black opacity-50" onClick={handleCloseUploadModal}></div>
+                <div className="bg-gray-200 p-6 rounded-lg shadow-lg z-10 w-full max-w-md mx-auto relative">
+                  <button
+                    onClick={handleCloseUploadModal}
+                    className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-600 p-2 rounded-full"
+                  >
+                    X 
+                  </button>
+                  <form onSubmit={handleSubmit} className="flex flex-col items-center">
+                    <input
+                      type="text"
+                      name="Nombre"
+                      value={formData.Nombre}
+                      onChange={handleInputChange}
+                      placeholder="Nombre"
+                      required
+                      className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
+                    />
+                    <input
+                      type="text"
+                      name="Descripcion"
+                      value={formData.Descripcion}
+                      onChange={handleInputChange}
+                      placeholder="Descripción"
+                      required
+                      className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
+                    />
+                    <input
+                      type="file"
+                      name="image"
+                      onChange={handleFileChange}
+                      required
+                      className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
+                    />
+                    {selectedFile && (
+                      <div className="text-center mb-4">
+                        <img
+                          src={URL.createObjectURL(selectedFile)}
+                          alt="Vista previa"
+                          className="max-h-48 border border-gray-300 rounded-lg"
+                        />
+                      </div>
+                    )}
+                    <button type="submit" className="w-full px-4 py-2 bg-gradient-to-tr from-gray-900 to-gray-800 text-white hover:bg-gradient-to-tr hover:from-gray-800 hover:to-gray-700 rounded-lg transition duration-300">
+                      Subir Foto
+                    </button>
+                  </form>
 
-      {selectedImage && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
-          <div className="relative bg-white p-4 rounded-lg">
-            <button
-              onClick={handleCloseModal}
-              className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-600 p-2 rounded-full"
-            >
-              X
-            </button>
-            <img
-              src={selectedImage}
-              alt="Ampliada"
-              width={400}
-              height={300}
-            />
-          </div>
-        </div>
-      )}
+                  {showConfirm && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50">
+                      <div className="fixed inset-0 bg-black opacity-50"></div>
+                      <div className="bg-gray-200 p-6 rounded-lg shadow-lg z-10 w-full max-w-md mx-auto">
+                        <h2 className="text-xl font-semibold mb-4">Confirmación de subida</h2>
+                        <p className="mb-4">¿Estás seguro de que quieres subir esta foto?</p>
+                        <div className="flex justify-end">
+                          <button
+                            onClick={() => setShowConfirm(false)}
+                            className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-700 transition duration-300 mr-2"
+                          >
+                            Cancelar
+                          </button>
+                          <button
+                            onClick={handleConfirmUpload}
+                            className="px-4 py-2 bg-gradient-to-tr from-gray-900 to-gray-800 text-white hover:bg-gradient-to-tr hover:from-gray-800 hover:to-gray-700 rounded-lg"
+                          >
+                            Confirmar
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
 
-      {showConfirmDelete && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="fixed inset-0 bg-black opacity-50"></div>
-          <div className="bg-gray-200 p-6 rounded-lg shadow-lg z-10 w-full max-w-md mx-auto">
-            <h2 className="text-xl font-semibold mb-4">Confirmación de eliminación</h2>
-            <p className="mb-4">¿Estás seguro de que quieres eliminar esta foto?</p>
-            <div className="flex justify-end">
-              <button
-                onClick={() => setShowConfirmDelete(false)}
-                className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-700 transition duration-300 mr-2"
-              >
-                Cancelar
-              </button>
-              <button
-                onClick={handleConfirmDelete}
-                className="px-4 py-2 bg-gradient-to-tr from-gray-900 to-gray-800 text-white hover:bg-gradient-to-tr hover:from-gray-800 hover:to-gray-700 rounded-lg"
-              >
-                Confirmar
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {showUploadModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="fixed inset-0 bg-black opacity-50" onClick={handleCloseUploadModal}></div>
-          <div className="bg-gray-200 p-6 rounded-lg shadow-lg z-10 w-full max-w-md mx-auto relative">
-            <button
-              onClick={handleCloseUploadModal}
-              className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-600 p-2 rounded-full"
-            >
-              X
-            </button>
-            <form onSubmit={handleSubmit} className="flex flex-col items-center">
-              <input
-                type="text"
-                name="Nombre"
-                value={formData.Nombre}
-                onChange={handleInputChange}
-                placeholder="Nombre"
-                required
-                className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
-              />
-              <input
-                type="text"
-                name="Descripcion"
-                value={formData.Descripcion}
-                onChange={handleInputChange}
-                placeholder="Descripción"
-                required
-                className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
-              />
-              <input
-                type="file"
-                name="image"
-                onChange={handleFileChange}
-                required
-                className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-lg"
-              />
-              {selectedFile && (
-                <div className="text-center mb-4">
+            {selectedImage && (
+              <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
+                <div className="relative bg-white p-4 rounded-lg">
+                  <button
+                    onClick={handleCloseModal}
+                    className="absolute top-2 right-2 text-white bg-gray-800 hover:bg-gray-600 p-2 rounded-full"
+                  >
+                    X
+                  </button>
                   <img
-                    src={URL.createObjectURL(selectedFile)}
-                    alt="Vista previa"
-                    className="max-h-48 border border-gray-300 rounded-lg"
+                    src={selectedImage}
+                    alt="Ampliada"
+                    width={400}
+                    height={300}
                   />
                 </div>
-              )}
-              <button type="submit" className="w-full px-4 py-2 bg-gradient-to-tr from-gray-900 to-gray-800 text-white hover:bg-gradient-to-tr hover:from-gray-800 hover:to-gray-700 rounded-lg transition duration-300">
-                Subir Foto
-              </button>
-            </form>
+              </div>
+            )}
 
-            {showConfirm && (
+            {showConfirmDelete && (
               <div className="fixed inset-0 flex items-center justify-center z-50">
                 <div className="fixed inset-0 bg-black opacity-50"></div>
                 <div className="bg-gray-200 p-6 rounded-lg shadow-lg z-10 w-full max-w-md mx-auto">
-                  <h2 className="text-xl font-semibold mb-4">Confirmación de subida</h2>
-                  <p className="mb-4">¿Estás seguro de que quieres subir esta foto?</p>
+                  <h2 className="text-xl font-semibold mb-4">Confirmación de eliminación</h2>
+                  <p className="mb-4">¿Estás seguro de que quieres eliminar esta foto?</p>
                   <div className="flex justify-end">
                     <button
-                      onClick={() => setShowConfirm(false)}
+                      onClick={() => setShowConfirmDelete(false)}
                       className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-700 transition duration-300 mr-2"
                     >
                       Cancelar
                     </button>
                     <button
-                      onClick={handleConfirmUpload}
+                      onClick={handleConfirmDelete}
                       className="px-4 py-2 bg-gradient-to-tr from-gray-900 to-gray-800 text-white hover:bg-gradient-to-tr hover:from-gray-800 hover:to-gray-700 rounded-lg"
                     >
                       Confirmar
@@ -296,9 +311,9 @@ export function Fotos() {
                 </div>
               </div>
             )}
-          </div>
-        </div>
-      )}
+          </>
+        )}
+      </div>
 
       <ToastContainer />
     </main>
