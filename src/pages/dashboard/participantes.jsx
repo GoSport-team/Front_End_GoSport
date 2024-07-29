d890ecee2f0b596fca11626becf1e94f5fe5bf98
yesicaz <ymzemanate@soy.sena.edu.co>
2024-07-29 17:23:12 -0500

    se agregaron pagina participantes estrcutura inicial

--- src/pages/dashboard/participantes.jsx
+++ src/pages/dashboard/participantes.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
-
-export const Participantes = () => {
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+    Avatar,
+    Chip,
+    Tooltip,
+    Progress,
+  } from "@material-tailwind/react";
+import Participantes from '@/widgets/componentes/Participantes/index'
+export const Participante= () => {
   return (
-    <div>participantes</div>
+   <>
+<Participantes/>
+<div className="flex justify-center space-x-4 mb-4">
+    <button
+      className="flex items-center justify-center text-white gap-1 px-5 py-3 cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-800 text-white px-4 py-2 rounded tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3"
+    >
+      Agregar Equipo
+    </button>
+    <button
+      className="flex items-center justify-center text-white gap-1 px-5 py-3 cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-800 text-white px-4 py-2 rounded tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3"
+    >
+      Sortear
+    </button>
+  </div>
+   </>
   )
 }
