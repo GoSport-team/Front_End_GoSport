1cf58109a59154766501a91bb1e69b9e3aa10555
maryyuu <charigdlopez@gmail.com>
2024-07-29 16:37:32 -0500

    Cambio en Editar cuenta

--- src/pages/campeonatos/Participantes.jsx
+++ src/pages/campeonatos/Participantes.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Participantes from "../../widgets/componentes/Participantes";
 export const Participantes = () => {
   return (
-    <div>Participantes</div>
+    <div>
+      <Participantes/>
+    </div>
   )
 }
