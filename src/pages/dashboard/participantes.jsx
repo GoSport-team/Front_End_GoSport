56412fb997f7c1d214cf10d7620ffbb8c3700301
yesicaz <ymzemanate@soy.sena.edu.co>
2024-07-31 08:29:12 -0500

    sorteo de equipos

--- src/pages/dashboard/participantes.jsx
+++ src/pages/dashboard/participantes.jsx
@@ -13,8 +13,13 @@ import {
 } from '@material-tailwind/react';
 
 export const Participante = () => {
+  const IdCampeonato = localStorage.getItem('ID');
   const [equipoInscripto, setEquipoInscripto] = useState([]);
-
+  const [estadoBoton, setEstadoBoton]=useState(true)
+  const [estadoFase, setEStadoFase]= useState(true)
+  const [nombreFase, setNombreFase]= useState("Fase 1")
+  const [idFases, setIdFase]= useState()
+  const [error, setError] = useState("");
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -25,6 +30,11 @@ export const Participante = () => {
           },
         });
         setEquipoInscripto(data);
+        if(data.length>=3){
+          setEstadoBoton(true)
+        }else{
+          setEstadoBoton(false)
+        }
       } catch (error) {
         console.log(error);
       }
@@ -32,13 +42,40 @@ export const Participante = () => {
     fetchData();
   }, []);
 
+
+
+  const sortearEquipos = async()=>{
+    try{
+      localStorage.setItem('estadoFase', estadoFase)
+      const fase = await axios.post('http://localhost:3001/fase', { estado:estadoFase,nombre: nombreFase, idCampeonato:IdCampeonato });
+      const idFase = fase.data._id;
+      setIdFase(idFase) 
+      const dataVs = {
+        equipos: equipoInscripto,
+        IdFase: idFase
+      };
+      localStorage.setItem('IdFase', idFase)
+      const equiposSorteados=  await axios.post('http://localhost:3001/vs',{dataVs:dataVs})
+      console.log(equiposSorteados)
+      if(equiposSorteados.data){
+        setEstadoBoton(false)
+      }
+     
+    }catch(error){
+      console.log(error)
+      setError("Error al sortear. Intente nuevamente.");
+    }
+  }
+ 
+
+
   return (
     <>
       <div className="p-4 ">
         {equipoInscripto && equipoInscripto.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-0 ">
             {equipoInscripto.map((equipo) => (
-              <div key={equipo.id} className="flex justify-center items-center z-0 ">
+              <div key={equipo._id} className="flex justify-center items-center z-0 ">
                 <Participantes equipo={equipo} />
               </div>
             ))}
@@ -51,16 +88,26 @@ export const Participante = () => {
       </div>
       <div className="flex justify-center space-x-4 mx-6 my-4">
         <button
+        
           className="flex items-center justify-center text-white gap-1 px-5 py-3 cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-800 text-white px-4 py-2 rounded tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3"
         >
           Agregar Equipo
         </button>
+        { estadoBoton &&(
         <button
+        onClick={sortearEquipos}
           className="flex items-center justify-center text-white gap-1 px-5 py-3 cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-800 text-white px-4 py-2 rounded tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3"
         >
           Sortear
         </button>
+        )}
+       
       </div>
+      {error&& (
+                <div className="text-red-600 text-center font-bold">
+                    {error}
+                </div>
+            )}
     </>
   );
 };
