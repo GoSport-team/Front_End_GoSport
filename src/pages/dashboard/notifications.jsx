fa339d81a5a1f9c68b71421a86ed1f030abaf679
maryyuu <charigdlopez@gmail.com>
2024-08-02 00:30:36 -0500

    Cronograma-VS

--- src/pages/dashboard/notifications.jsx
+++ src/pages/dashboard/notifications.jsx
@@ -7,8 +7,12 @@ import {
   CardBody,
 } from "@material-tailwind/react";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
-import CronogramaDesing from "@/widgets/componentes/Cronograma";
-
+//CRONOGRAMA
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CronogramaDesing from "@/widgets/componentes/Cronograma/index";
+ 
+import { ConfirmarGuardar } from "@/widgets/componentes/Cronograma/ConfirmarGuardar";
 export function Notifications() {
   const [showAlerts, setShowAlerts] = React.useState({
     blue: true,
@@ -23,10 +27,78 @@ export function Notifications() {
     red: true,
   });
   const alerts = ["gray", "green", "orange", "red"];
+  //Cronograma Borrar
+  const [datosVss, setDatosVs]= useState();
+  const [fechaHora, setFechaHora]= useState({'fecha':'', 'hora':''})
 
+  const [confirmarCambios, setConfirmarCambios] = useState(false);
+  const [IdVs , setIdVs]= useState('')
+  //console.log(datosVss)
+    const IdFasee = localStorage.getItem('IdFase');
+  
+    useEffect(()=>{
+      const GetDatosVs = async()=>{
+        try{
+          const Vs= await axios.get('http://localhost:3001/vs',{
+            headers:{
+              IdFase:IdFasee
+            }
+          });
+          setDatosVs(Vs.data)
+          console.log(Vs.data)
+          console.log(Vs.data)
+          console.log(IdFasee)
+        }catch(error){
+          console.log(error)
+        }
+      }
+      GetDatosVs();
+    },[])
+
+    const hanlde = (estado,idVs)=>{
+      setConfirmarCambios(estado)
+      setIdVs(idVs)
+    }
+    
   return (
     <>
-      <CronogramaDesing />
+     {/* //CRONOGRAMA */}
+     <div className='w-full h-full'>
+      <Card className='w-auto h-auto flex justify-center items-center p-2'>
+            <div  className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-7">
+              { datosVss && datosVss.map((versus)=>(
+              <div  key={versus._id} className="flex justify-center items-center z-0">
+                <CronogramaDesing
+                primerEquipo={versus.equipo1.informacion.team1.Equipo.nombreEquipo}
+                imagenEquipo1={versus.equipo1.informacion.team1.Equipo.imgLogo}
+                segundoEquipo={versus.equipo2.informacion.team2.Equipo.nombreEquipo}
+                imagenEquipo2={versus.equipo2.informacion.team2.Equipo.imgLogo}
+                idVs={versus._id}
+                patchFechaHora={setFechaHora}
+
+                guardarEdicion={hanlde}
+                ></CronogramaDesing>
+
+              </div>
+              ))}
+              
+             
+            </div>
+            
+            {
+                confirmarCambios && (
+                  <ConfirmarGuardar
+                  confirmarCambios={setConfirmarCambios}
+                  idVs={IdVs}
+                  cerrarModal={setConfirmarCambios}
+                  fecha={fechaHora.fecha}
+                  hora={fechaHora.hora}
+                  />
+                )
+              }
+       
+      </Card>
+    </div>  
       {/* Para prueba Nomas */}
     </>
     // <div className="mx-auto my-20 flex max-w-screen-lg flex-col gap-8">
--- src/pages/dashboard/tables.jsx
+++ src/pages/dashboard/tables.jsx
@@ -63,6 +63,7 @@ export function Tables() {
     
 
   return (
+    <>
     <div className="mt-10 mb-8 flex flex-col">
     <div className="flex justify-start">
       <button className="flex items-center justify-center text-white gap-1 px-5 py-3 cursor-pointer bg-gradient-to-tr from-gray-900 to-gray-800 text-white px-4 py-2 rounded tracking-widest rounded-md duration-300 hover:gap-2 hover:translate-x-3">
@@ -143,7 +144,9 @@ export function Tables() {
       onUpdate={idUpdate}
     />
   </div>
-  );
+    </>
+      );
+
 }
 
 export default Tables;
