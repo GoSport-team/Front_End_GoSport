a55e8872fdb39193853f95c3b189702c372527cf
Yerson <Dreyerg9@gmail.com>
2024-08-02 16:45:03 -0500

    Cambios Loanding y Resultado

--- src/widgets/componentes/Cronograma/index.jsx
+++ src/widgets/componentes/Cronograma/index.jsx
@@ -9,8 +9,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';     
-export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEquipo1, imagenEquipo2,idVs, patchFechaHora, guardarEdicion}) {
+import 'react-toastify/dist/ReactToastify.css';
+export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEquipo1, imagenEquipo2, idVs, patchFechaHora, guardarEdicion }) {
+
+  const [datos, setDatos] = useState([]);
 
   useEffect(() => {
     const fetchFechaHora = async () => {
@@ -19,6 +21,8 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
         const { fecha, hora } = response.data;
         setFecha(fecha || '');
         setHora(hora || '');
+        console.log(response.data);
+
       } catch (error) {
         console.error("Error al obtener fecha y hora:", error);
       }
@@ -27,21 +31,39 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
     fetchFechaHora();
   }, [idVs]);
 
-  const handleConfirmarCmabios = () =>{
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/vs/${idVs}`);
+        setDatos(response.datos)
+        console.log("Resultados", response.data);
+
+      } catch (error) {
+        console.error("Error al obtener Datos", error);
+      }
+    };
+
+    fetchData();
+  }, [idVs]);
+
+
+
+  const handleConfirmarCmabios = () => {
     guardarEdicion(true, idVs);
-    patchFechaHora({fecha,hora})
+    patchFechaHora({ fecha, hora })
   }
   const [fecha, setFecha] = useState('');
   const [hora, setHora] = useState('');
 
-  const handleFecha = (e) =>{
+  const handleFecha = (e) => {
     setFecha(e.target.value)
   }
-  const handleHora =(e)=>{
+  const handleHora = (e) => {
     setHora(e.target.value)
   }
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);                                                    
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -72,21 +94,21 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
       <div className='w-[35vw] h-72 mt-8 flex flex-row rounded-md border-2 border-gray-300 shadow-lg'>
         <div className='w-1/2 h-full p-6 flex flex-col items-center gap-6'>
           <div className='w-full flex flex-row '>
-            <img className='w-2/4 object-contain h-24 rounded-3xl ' src={imagenEquipo1} />
+            <img className='w-2/4 object-contain h-24 rounded-[50%] ' src={imagenEquipo1} />
             <div className='w-full flex justify-center items-center'>
-            <h4 className='text-xl font-semibold text-gray-700'>{primerEquipo}</h4>
+              <h4 className='text-xl font-semibold text-gray-700'>{primerEquipo}</h4>
             </div>
-           
+
           </div>
           <div className='w-full flex flex-row gap-4'>
-            <img className='w-2/4 object-contain h-24 rounded-3xl' src={imagenEquipo2} />
+            <img className='w-2/4 object-contain h-24 rounded-[50%] ' src={imagenEquipo2} />
             <div className='w-full flex justify-center items-center'>
-            <h4 className=' text-xl font-semibold text-gray-700'>{segundoEquipo}</h4>
+              <h4 className=' text-xl font-semibold text-gray-700'>{segundoEquipo}</h4>
             </div>
-           
+
           </div>
         </div>
-        
+
         <div className='w-1/2 p-6 flex flex-col'>
           <div className='mb-6'>
             <h3 className='text-2xl font-bold text-gray-800'>Detalles</h3>
@@ -97,15 +119,18 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
               <input type="time" value={hora} onChange={handleHora} className='p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
             </div>
             <div className='flex items-center gap-x-10'>
-              <label  className='text-lg font-medium text-gray-600'>Fecha</label>
+              <label className='text-lg font-medium text-gray-600'>Fecha</label>
               <input type="date" value={fecha} onChange={handleFecha} className='p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
             </div>
           </div>
           <div className='mt-6 flex justify-between'>
-            <button onClick={openModal} className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'>Resulatdos</button>
+            <button onClick={openModal} className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'>Resulaldos</button>
             <button onClick={handleConfirmarCmabios} className='px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400'>Editar</button>
           </div>
         </div>
+      </div>
+      <div>
+        
       </div>
 
       <Modal
@@ -131,11 +156,11 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
                   <div className='flex content-center justify-center gap-x-5'>
                     <div className='grid place-content-center'>
                       <div>
-                        <img className="object-contain w-32 drop-shadow-lg"
-                          src="\public\img\Resultados\Madrid.png" alt="img" />
+                        <img className="object-contain w-32 drop-shadow-lg rounded-[50%] "
+                          src={imagenEquipo1}  alt="img" />
                       </div>
                       <div className='flex justify-center content-center'>
-                        <h1 className="my-2">Real Madrid</h1>
+                        <h1 className="my-2">{primerEquipo}</h1>
                       </div>
                     </div>
                     <div className='grid place-content-center text-4xl p-4 '>
@@ -213,11 +238,11 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
                   <div className='flex content-center justify-center gap-x-5'>
                     <div className='grid place-content-center'>
                       <div>
-                        <img className="object-contain w-32 drop-shadow-lg"
-                          src="\public\img\Resultados\Dourmut.png" alt="img" />
+                        <img className="object-contain w-32 drop-shadow-lg  rounded-[50%] "
+                          src={imagenEquipo2} alt="img" />
                       </div>
                       <div className='flex justify-center content-center'>
-                        <h1 className="my-2">Dourmut</h1>
+                        <h1 className="my-2">{segundoEquipo}</h1>
                       </div>
                     </div>
                     <div className='grid place-content-center text-4xl p-4'>
@@ -285,7 +310,46 @@ export default function CronogramaDesing({ primerEquipo, segundoEquipo, imagenEq
           </section>
         </div>
       </Modal>
-     
+
+      {/* <div className='flex w-[40vw] '>
+        <h1>Notifications</h1>
+        {data.length > 0 ? (
+          data.map((item) => (
+            <div key={item._id}>
+              <h2>Match Information</h2>
+              <div>
+                <h3>Team 1</h3>
+                <p><strong>Equipo 1:</strong> {item.equipo1.Equipo1.nombreEquipo}</p>
+                <p><strong>Captain:</strong> {item.equipo1.Equipo1.nombreCapitan}</p>
+                <p><strong>Jornada:</strong> {item.equipo1.Equipo1.jornada}</p>
+                <p><strong>Goals Team 1:</strong> {item.equipo1.goles.join(', ')}</p>
+                <p><strong>Highlighted Player Team 1:</strong> {item.equipo1.jugadorDestacado.nombres}</p>
+
+                <h4>Team 1 Players:</h4>
+                <ul>
+                  {item.equipo1.Equipo1.participantes.map((player) => (
+                    <li key={player._id}>{player.nombreJugador} - Dorsal: {player.dorsal}</li>
+                  ))}
+                </ul>
+
+                <h3>Team 2</h3>
+                <p><strong>Equipo 2</strong> {item.equipo2.Equipo2.nombreEquipo}</p>
+                <p><strong>Captain:</strong> {item.equipo2.Equipo2.nombreCapitan}</p>
+                <p><strong>Jornada:</strong> {item.equipo2.Equipo2.jornada}</p>
+
+                <h4>Team 2 Players:</h4>
+                <ul>
+                  {item.equipo2.Equipo2.participantes.map((player) => (
+                    <li key={player._id}>{player.nombreJugador} - Dorsal: {player.dorsal}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div> */}
     </>
   )
 }
