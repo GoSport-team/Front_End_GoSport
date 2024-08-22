c004a4d81fce517f4fe4287998dc2c67782b9e7b
10-Yerson <Dreyerg9@gmail.com>
2024-08-22 09:39:26 -0500

    Botones campeonato

--- src/widgets/componentes/campeonato/estadosCampeonato/creado.jsx
+++ src/widgets/componentes/campeonato/estadosCampeonato/creado.jsx
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
       <form onSubmit={handleSubmit}>
         <button
           type="submit"
-          class="select-none rounded-lg bg-[#12aed1cd] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          class="w-44 select-none rounded-lg bg-[#12aed1cd] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         >
           Publicar
         </button>
--- src/widgets/componentes/campeonato/estadosCampeonato/ejecucion.jsx
+++ src/widgets/componentes/campeonato/estadosCampeonato/ejecucion.jsx
@@ -37,7 +37,7 @@ export const Ejecucion = ({tasks}) => {
 {ejecucion &&(
     <div>
        <button onClick={()=>handleClick()}
-        class="select-none rounded-lg bg-[#12aed1cd] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+        class="w-44 select-none rounded-lg bg-[#12aed1cd] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
             <Link  to="/campe/cronograma">Agregar Resultados</Link>
         </button>
     </div>
