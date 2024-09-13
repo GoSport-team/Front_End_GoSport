2b494c07555574d3d073088d466c25194996ca48
Luisa Fernanda <lufvi02@gmail.com>
2024-09-13 00:50:42 -0500

    modal mostrar jugadores corregi campo nombreJugador por nombres

--- src/widgets/componentes/Cronograma/mostrarJugadores.jsx
+++ src/widgets/componentes/Cronograma/mostrarJugadores.jsx
@@ -276,7 +276,7 @@ export const MostrarJugadores = ({datosVss,setModalIsOpen, modalIsOpen, closeMod
                             {equipo1 && equipo1.participantes.map((jugador)=>(
                                 <tr key={jugador._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {jugador.nombreJugador}
+                                    {jugador.nombres}
                                     </th>
                                     <td className="px-6 py-4">{jugador.dorsal}</td>
                                     <td className="px-6 py-4">
@@ -390,7 +390,7 @@ export const MostrarJugadores = ({datosVss,setModalIsOpen, modalIsOpen, closeMod
                               {equipo2.participantes ? equipo2.participantes.map((jugador)=>(
                                 <tr key={jugador._id}className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {jugador.nombreJugador}
+                                        {jugador.nombres}
                                     </th>
                                     <td className="px-6 py-4">{jugador.dorsal}</td>
                                     <td className="px-6 py-4">
